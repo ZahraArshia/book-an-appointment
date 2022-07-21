2378a31874604d5814b32929d8a88f2b3d911882
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-07-21 23:22:07 +0430

    fix linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,14 @@ class ApplicationController < ActionController::Base
   # end
 
   def authenticate_user
-    if request.headers['Authorization'].present?
-      authenticate_or_request_with_http_token do |token|
-        jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+    return unless request.headers['Authorization'].present?
 
-        @current_user_id = jwt_payload['id']
-      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
-        head :unauthorized
-      end
+    authenticate_or_request_with_http_token do |token|
+      jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+      @current_user_id = jwt_payload['id']
+    rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+      head :unauthorized
     end
   end
 
