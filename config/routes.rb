42cbc246a8facde95b344e24619e1a1de15bb5fc
nevisende <ekonomitakipmail@gmail.com>
2022-07-23 12:31:10 +0300

    Minor: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-     namespace :api, defaults: { format: 'json' } do
-      namespace :v1, defaults: { format: 'json' } do
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+  
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1, defaults: { format: 'json' } do
         resources :reservations, only: [:index, :create, :destroy]
         resources :users, only: [:index, :create]
         resources :items, only: [:index, :new, :create, :update, :destroy]
