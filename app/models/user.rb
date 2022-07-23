eb029ada67707ce74c7a17132d15261940e02141
nevisende <ekonomitakipmail@gmail.com>
2022-07-23 12:30:45 +0300

    Feat: validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_many :reserved_items, through: :reservations, source: :item
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
